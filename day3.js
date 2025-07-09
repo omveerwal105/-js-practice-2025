3efb32d962b737d5d48219922d4f96100a3f604a
Om Veerwal <omveerwal9@gmail.com>
2025-07-09 23:34:41 +0530

    factorial and countVowels

---
+++ day3.js
@@ -0,0 +1,23 @@
+function factorial(n) {
+  let result = 1;
+
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+console.log(factorial(5)); 
+
+function countVowels(str) {
+    let count =0;
+    let vowels =  "aeiou";
+    for(let char of str.toLowerCase() ){
+        if(vowels.includes(char)){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels("chatGPT is cool"));
